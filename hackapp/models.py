09407f5ced1127e9ce8c45f99fffcf77d42265c1
alexnadeev <anadeev83@gmail.com>
2019-10-16 10:39:33 -0500

    progress

--- hackapp/models.py
+++ hackapp/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Users(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # user_name = models.CharField(max_length=15)
-    # email = models.CharField(max_length=50)
-    # password = models.CharField(max_length=50)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # vote_count = models.IntegerField(default=0)
+# class Users(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     # user_name = models.CharField(max_length=15)
+#     # email = models.CharField(max_length=50)
+#     # password = models.CharField(max_length=50)
+#     # created_at = models.DateTimeField(auto_now_add=True)
+#     # vote_count = models.IntegerField(default=0)
 
 class Post(models.Model):
-    author = models.ForeignKey('Users', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     body = models.TextField(max_length=5000)
     vote_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
-    author = models.ForeignKey('Users', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField(max_length=5000)
     vote_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
